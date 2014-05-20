a802e153828dd6d52042bcc6b115139aa7030328
Paul Wenig <pwenig@gmail.com>
2014-05-19 22:04:23 -0600

    Centers text

--- lib/text_center.rb
+++ lib/text_center.rb
@@ -1,12 +1,13 @@
 class TextCenter
 
-  def initialize(string)
-    @string = string
-    
+  def initialize(text)
+    @text = text
   end
 
-  def center_the_string
-    []
+  def centered
+    line_array = @text.lines
+    longest_line_count = line_array.sort{|x, y| y.length <=> x.length}.first.length
+    line_array.map {|line| line.center(longest_line_count).rstrip }.join("\n")
 
   end
 end
--- spec/text_center_spec.rb
+++ spec/text_center_spec.rb
@@ -3,31 +3,21 @@ require 'text_center'
 
 describe TextCenter do
 
-  it "determines if string is centered" do
-
-    string = ["This is a short line", "This is a really long line",
-    "and this is", "short"]
-
-  #  New up a class that takes a string
-    text_center = TextCenter.new(string)
-
-    text_center = []
-    result = text_center.center_the_sting(string)
-
-  #   create a method which fines the longest string
-    # and compares it to every other line. Add padding
-  #   so it's centered against the longest line
-  #   adds padding to every other line so that it's centered
-
-
-    #return the centered string
-    expect(result).to eq <<HEREDOC
-   "This is a short line",
-"This is a really long line",
-    "and this is",
-         "short"
-HEREDOC
-
-
+  it "takes a string and returns a string with the text centered against the longest line" do
+    input = <<-INPUT
+this is a short line
+this is a really long line that has a lot of characters
+and this is
+short
+    INPUT
+    text_input = TextCenter.new(input)
+    expected = <<-EXPECTED.chomp
+                 this is a short line
+this is a really long line that has a lot of characters
+                      and this is
+                         short
+    EXPECTED
+    expect(text_input.centered).to eq(expected)
   end
+
 end
